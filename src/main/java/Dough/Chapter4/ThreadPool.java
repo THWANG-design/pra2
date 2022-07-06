aa1a6163c06e69b3f7d831885d8d226233cf46f0
Taohan Wang <2520367w@student.gla.ac.uk>
2022-07-06 11:30:12 +0800

    Youtijiaole

--- src/main/java/Dough/Chapter4/ThreadPool.java
+++ src/main/java/Dough/Chapter4/ThreadPool.java
@@ -10,7 +10,7 @@ public class ThreadPool {
     public static void main(String[] args) {
         BlockingQueue<Runnable>bq = new LinkedBlockingQueue<>(5);
         ThreadPoolExecutor pool = new ThreadPoolExecutor(3,8,2000, TimeUnit.MILLISECONDS,bq);
-
+        System.out.println("我实在master分支上进行了修改");
         for (int i = 0; i < 10; i++) {
             pool.execute(new Runnable() {
                 @Override

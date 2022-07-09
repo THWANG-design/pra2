4e60391b654289e8de63c078fd78dff8d20c7d33
Taohan Wang <2520367w@student.gla.ac.uk>
2022-07-09 11:47:53 +0800

    C2

--- src/main/java/Dough/Chapter4/ThreadPool.java
+++ src/main/java/Dough/Chapter4/ThreadPool.java
@@ -15,9 +15,7 @@ public class ThreadPool {
         System.out.println("C2");
 
 
-        System.out.println("C8");
-        System.out.println("C9");
-        
+
 
         
         for (int i = 0; i < 10; i++) {
